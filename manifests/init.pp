fb7aed5a584b9a3a3d21d3a3ee65cdeb51959fa3
Marcel Haerry <haerry@puzzle.ch>
2008-05-28 18:33:45 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# tftp module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "tftp": }
+class tftp {}
