adfa5b67411e20d9bad95b56ba361166d587b08c
haerry@puzzle.ch <mhaerry@pw-mhaerry.puzzle.itc>
2008-05-28 22:19:31 +0200

    fixed url

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@ class tftp {
     }
 
     file { '/etc/xinetd.d/tftp':
-        source => [ "server/tftp/tftp.xinetd.$operatingsystem",
+        source => [ "puppet://$server/tftp/tftp.xinetd.$operatingsystem",
                     "puppet://$server/tftp/tftp.xinetd" ],
         require => Package['tftp-server'],
         owner => root, group => 0, mode => '0644';
