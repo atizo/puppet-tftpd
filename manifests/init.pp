7eab5299ff8321b2f24923092adf458ecd5cb16d
Marcel Haerry <haerry@puzzle.ch>
2008-05-28 18:40:56 +0200

    added definitions

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,17 @@
 # GPLv3
 #######################################
 
+class tftp {
 
-# modules_dir { "tftp": }
-class tftp {}
+    package { 'tftp-server':
+        ensure => 'present',
+    }
+
+    file { '/etc/xinetd.d/tftp':
+        source => [ "server/tftp/tftp.xinetd.$operatingsystem",
+                    "puppet://$server/tftp/tftp.xinetd" ],
+        require => Package['tftp-server']
+        owner => root, group => 0, mode => '0644';
+    }
+
+}
