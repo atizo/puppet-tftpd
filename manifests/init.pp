4148149064aacef2a83af007e4b68f041336c81d
Simon Josi <simon.josi@atizo.com>
2011-10-11 13:44:13 +0200

    call it tftpd instead of tftp

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,10 +17,10 @@ class tftpd {
   package{'tftp-server':
     ensure => present,
   }
-  file{'/etc/xinetd.d/tftp':
+  file{'/etc/xinetd.d/tftpd':
     source => [
-        "puppet://$server/tftp/tftp.xinetd.$operatingsystem",
-        "puppet://$server/tftp/tftp.xinetd",
+        "puppet://$server/tftpd/tftp.xinetd.$operatingsystem",
+        "puppet://$server/tftpd/tftp.xinetd",
     ],
     require => Package['tftp-server'],
     owner => root, group => 0, mode => 0644;
