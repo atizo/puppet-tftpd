de6fe81ae5e30466cbee2193972f5db0619c2e43
Simon Josi <simon.josi@atizo.com>
2011-10-11 10:47:06 +0200

    update copyright info

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,8 @@
 # Copyright 2008, Puzzle ITC GmbH
 # Marcel Härry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
+# Copyright 2011, Atizo AG
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -12,16 +14,15 @@
 #
 
 class tftp {
-
-    package { 'tftp-server':
-        ensure => 'present',
-    }
-
-    file { '/etc/xinetd.d/tftp':
-        source => [ "puppet://$server/tftp/tftp.xinetd.$operatingsystem",
-                    "puppet://$server/tftp/tftp.xinetd" ],
-        require => Package['tftp-server'],
-        owner => root, group => 0, mode => '0644';
-    }
-
+  package{'tftp-server':
+    ensure => present,
+  }
+  file{'/etc/xinetd.d/tftp':
+    source => [
+        "puppet://$server/tftp/tftp.xinetd.$operatingsystem",
+        "puppet://$server/tftp/tftp.xinetd",
+    ],
+    require => Package['tftp-server'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
